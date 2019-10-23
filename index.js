01ae66e64deae2f803ce50e654c952173204bc4b
Hugo Haldi <20846785+ShinoYasx@users.noreply.github.com>
2019-10-23 10:58:41 +0200

    Return Promise

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 const axios = require('axios');
 
 var virthttp = {};
-virthttp.req = (target) => {
-    if (!target)
-        target = '';
+virthttp.req = (target, params) => {
+    target = target || '';
     return {
         libvirt: () => {
             return {
                 domain: (selector) => {
-                    if (!selector)
-                        throw "No selector provided";
-                    if (selector.name && selector.uuid)
-                        throw "Only one selector must be set";
-                    if (selector.name)
-                        return axios.get(`${target ? target : ''}/libvirt/domains/by-name/${selector.name}`);
-                    if (selector.uuid)
-                        return axios.get(`${target}/libvirt/domains/by-uuid/${selector.uuid}`);
+                    return new Promise((resolve, reject) => {
+                        if (!selector)
+                            reject(new SyntaxError("No selector provided"));
+                        if (selector.name && selector.uuid)
+                            reject(new SyntaxError("More than one selector provided"));
+                        function get(target, type, selector) {
+                            return(axios.get(`${target}/libvirt/domains/by-${type}/${selector}`, params));
+                        }
+                        if (selector.name)
+                            resolve(get(target, 'name', selector.name));
+                        if (selector.uuid)
+                            resolve(get(target, 'uuid', selector.uuid));
+                        reject(new SyntaxError("Bad selector"));
+                    });
                 },
                 domains: () => {
-                    return axios.get(`${target}/libvirt/domains`);
+                    return axios.get(`${target}/libvirt/domains`, params);
                 }
             }
         }
     }
 }
-
-let req = virthttp.req().libvirt().domains;
-var result = req();
-result
-    .then((res) => {
-        console.log(res.data);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-
