1cfd884de39c287f461b84844e00fd46f1dfd56d
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-10-23 18:34:16 +0200

    Qfix: incorrect identifier
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 module.exports = require('./lib/virthttp');
 
 const virthttp = module.exports;
+
 let req = virthttp.req('http://127.0.0.1:8081').libvirt().domains();
 
 if(req.state !== 'ready')
     console.log(`error: ${req.err_msg}`);
 else
     req().then(res => {
+        console.log(res.data);
+    }).catch(err => console.error(err));
+
+let req2 = virthttp.req('http://127.0.0.1:8081').libvirt().domain({name: 'win10'});
+if(req2.state !== 'ready')
+    console.log(`error: ${req2.err_msg}`);
+else
+    req2().then(res => {
         console.log(res.data);
     }).catch(err => console.error(err));
